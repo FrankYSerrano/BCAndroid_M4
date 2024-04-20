6542ef52f78b5784b80400d10d33343f630c90dc
FrankYSerrano <frankyserrano@gmail.com>
2024-04-19 22:35:54 -0400

    Agregando Alkewallet 1 tarea del domingo 21 de abril

---
+++ AlkeWallet1/app/src/main/java/com/example/alkewallet1/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.alkewallet1;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.alkewallet_splash);
+    }
+}
